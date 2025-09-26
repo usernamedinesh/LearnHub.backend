c7ca5da885b468d966b1e924f964f6c32669ed58
usernamedinesh <borod9200@gmail.com>
2025-09-26 08:18:47 +0530

    removed db call from health controller

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,20 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { allUser } from 'src/database';
 
 @ApiTags('Health')
 @Controller('health')
 export class HealthController {
-  private readonly startTime = Date.now();
-
   @Get()
-  async checkHealth() {
-    const user = await allUser;
+  checkHealth() {
     return {
       status: 'ok',
       uptime: process.uptime(),
       timestamp: Date.now(),
-      user,
     };
   }
 }
