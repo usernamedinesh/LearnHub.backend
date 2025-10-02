b1458f1621e28fd1dae89dfa1c2d1123c02dc723
usernamedinesh <borod9200@gmail.com>
2025-10-02 23:00:12 +0530

    schema migration for tokens

--- src/schema/type.ts
+++ src/schema/type.ts
@@ -1,5 +1,11 @@
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { users } from "./users";
+
 export enum userRole {
     User = "user",
     Instructor = "instructor",
     Student = "student",
 }
+export type User = InferSelectModel<typeof users>;
+export type NewUser = InferInsertModel<typeof users>;
+export type SafeUser = Omit<NewUser, 'password'>;
